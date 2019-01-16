29ff163dd3aa1e2a262f62b8d04004c34d033c78
st52938 <st52938@student.upce.cz>
2019-01-16 22:13:23 +0100

    Portfolio DB

---
+++ page/reference.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('headSection.html') ?>
+</head>
+<body>
+<!--menu-->
+<header>
+    <?php include('mainNavbar.html') ?>
+</header>
+
+<main>
+    <p>Reference page </p>
+</main>
+</body>
+</html>
