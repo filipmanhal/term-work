a1a4aed86b631c78c34bfc6a4df55c3f74574c94
st52938 <st52938@student.upce.cz>
2019-01-17 20:17:49 +0100

    about.php

--- page/about.php
+++ page/about.php
@@ -10,39 +10,23 @@
     <?php include('mainNavbar.html') ?>
 </header>
 
-<section id="AboutCover">
+<section id="aboutCover">
     <h1>Kdo jsem...</h1>
-
 </section>
 
 <main>
-        <p>Sdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
-            aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq
-            wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw
-            dq wd waaaaaaaaaaaaaaaaaaajdajsd
-            ajsdilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsd
-            ilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
-            aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
-            aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq
-            wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw
-            dq wd waaaaaaaaaaaaaaaaaaajdajsd
-            ajsdilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdaj.
-        </p>
-    </main>
-<!--
-<main class="wrapper">
-    <section class="section parallax bg1">
-        <h1>Such Adorableness</h1>
-    </section>
-    <section class="section static">
-        <h1>Boring</h1>
-    </section>
-
-    <section class="section parallax bg2">
-        <h1>SO FWUFFY AWWW</h1>
-    </section>
+    <p>Sdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
+        aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq
+        wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw
+        dq wd waaaaaaaaaaaaaaaaaaajdajsd
+        ajsdilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsd
+        ilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
+        aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
+        aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq
+        wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw
+        dq wd waaaaaaaaaaaaaaaaaaajdajsd
+        ajsdilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdaj.
+    </p>
 </main>
--->
 </body>
-
 </html>
