7c6ff5847be2c7c998317bf8f039ad1228826080
st52938 <st52938@student.upce.cz>
2019-01-17 18:26:50 +0100

    pozadi, o mne

--- page/about.php
+++ page/about.php
@@ -16,16 +16,33 @@
 </section>
 
 <main>
-    <p>aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
-        aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq
-        wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw
-        dq wd waaaaaaaaaaaaaaaaaaajdajsd
-        ajsdilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsd
-        ilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
-    </p>
-</main>
-</body>
-<main>
+        <p>Sdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
+            aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq
+            wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw
+            dq wd waaaaaaaaaaaaaaaaaaajdajsd
+            ajsdilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsd
+            ilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
+            aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq wd w
+            aaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw dq
+            wd waaaaaaaaaaaaaaaaaaajdajsdajsdilqwdqwdqdqwd qwdqw
+            dq wd waaaaaaaaaaaaaaaaaaajdajsd
+            ajsdilqwdqwdqdqwd qwdqw dq wd waaaaaaaaaaaaaaaaaaajdajsdaj.
+        </p>
+    </main>
+<!--
+<main class="wrapper">
+    <section class="section parallax bg1">
+        <h1>Such Adorableness</h1>
+    </section>
+    <section class="section static">
+        <h1>Boring</h1>
+    </section>
 
+    <section class="section parallax bg2">
+        <h1>SO FWUFFY AWWW</h1>
+    </section>
 </main>
+-->
+</body>
+
 </html>
