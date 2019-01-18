75d8c92db1f033b232d2a6a1687b53a61764d13b
st52938 <st52938@student.upce.cz>
2019-01-19 00:16:41 +0100

    pozadi, o mne

--- page/Blog/admin/dashboard.php
+++ page/Blog/admin/dashboard.php
@@ -13,7 +13,7 @@
 		<?php if (isset($_SESSION['user'])): ?>
 			<div class="user-info">
 				<span><?php echo $_SESSION['user']['username'] ?></span> &nbsp; &nbsp; 
-				<a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
+				<a href="<?php echo BASE_URL . 'logout.php'; ?>" class="logout-btn">logout</a>
 			</div>
 		<?php endif ?>
 	</div>
